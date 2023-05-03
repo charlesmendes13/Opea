2e9f50ed516762c4704009858d5af00a87ca2bed
Charles Mendes <charlesluizmendes@gmail.com>
2023-05-03 03:16:42 -0300

    Alterando Middleware Error

--- src/Opea.Web/Program.cs
+++ src/Opea.Web/Program.cs
@@ -32,10 +32,11 @@ builder.Services.AddAutoMapper(typeof(ViewModelToDomainMappingProfile), typeof(D
 
 var app = builder.Build();
 
+app.UseExceptionHandler("/Home/Error");
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
